b3ccebf79ecd2261482c743d1c986ce783aa1527
Dipmallo Dey <dipmallodey1996@gmail.com>
2023-04-20 04:09:20 +0530

    initialized backendf and frontend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv =require('dotenv')
+
+const app = express();
+dotenv.config();
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  console.log(note);
+  res.send(note);
+});
+
+const PORT = process.env.PORT || 4300
+app.listen(PORT, console.log("server started"));
